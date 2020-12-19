c9a1f77d991ccfca8d4bff48378913dc01adb226
Wanderson Sales <wandersonsales.dev@gmail.com>
2020-12-18 23:14:31 -0300

    API de Usuários #2

--- knowledge/backend/api/article.js
+++ knowledge/backend/api/article.js
@@ -1,3 +1,5 @@
+const queries = require('./queries')
+
 module.exports = app => {
   const { existsOrError } = app.api.validation
 
@@ -68,8 +70,24 @@ module.exports = app => {
         article.content = article.content.toString()
         return res.json(article)
       })
-      .catch(err => res.status(500).send())
+      .catch(err => res.status(500).send(err))
   }
 
-  return { save, remove, get, getById }
+  const getByCategory = async (req, res) => {
+    const categoryId = req.params.id
+    const page = req.query.page || 1
+    const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
+    const ids = categories.rows.map(c => c.id)
+
+    app.db({ a: 'articles', u: 'users' })
+      .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+      .limit(limit).offset(page * limit - limit)
+      .whereRaw('?? = ??', ['u.id', 'a.userId'])
+      .whereIn('categoryId', ids)
+      .orderBy('a.id', 'desc')
+      .then(articles => res.json(articles))
+      .catch(err => res.status(500).send(err))
+  }
+
+  return { save, remove, get, getById, getByCategory }
 }
