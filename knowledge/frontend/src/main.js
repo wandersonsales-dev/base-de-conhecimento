9cfc883c677acedebf228c1df27e6e1a6539b25a
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-01-19 13:23:24 -0300

    Componente Home

--- knowledge/frontend/src/main.js
+++ knowledge/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORÁRIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIE0uIExlaXTDo28iLCJlbWFpbCI6Imxlb25hcmRvQGNvZDNyLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE2MTEwNzA4MzcsImV4cCI6MTYxMTMzMDAzN30.mwUAGs2TB_z9_IyTaWvDIrCQQsA8BJAncHvVQy88yRE'
+
 new Vue({
   store,
   router,
